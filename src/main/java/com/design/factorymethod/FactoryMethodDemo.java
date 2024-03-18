b9f0f10659bfa82b1e2902b918ebb174cc868b5c
coderkeep <coderkeep@gmail.com>
2024-03-18 23:14:51 +0800

    feat: 工厂方法模式补充

--- src/main/java/com/design/factorymethod/FactoryMethodDemo.java
+++ src/main/java/com/design/factorymethod/FactoryMethodDemo.java
@@ -3,6 +3,8 @@ package com.design.factorymethod;
 public class FactoryMethodDemo {
 
     public static void main(String[] args) {
+        // 通过 creator 封装工厂的公共执行逻辑
+        // 线执行 creator 中公共部分，再通过具体的工厂方法创建对象
         new ConcreteProductCreator().create();
     }
 }
